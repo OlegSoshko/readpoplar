4fdc0cc032b19750d45fbf2b7241f5e43e63bf1f
Oleg Soshko <oleg.91.03@gmail.com>
2022-06-23 19:51:44 +0300

    MainPage начало

---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MainPage: React.FC = () => {
+  return (
+    <div>
+      <div>HEADER</div>
+      <div>MAIN</div>
+      <div>FOOTER</div>
+    </div>
+  )
+}
